52504476bb96bc0baec6ea85c5138b67c2f044dc
Ilias Mavreas <ilias.mavreas@gmain.com>
2026-03-07 13:46:37 -0600

    deleted test test files

--- RunBench.ps1
+++
@@ -1,8 +0,0 @@
-Set-Location q:\code\projects\csharp\lisp\bin\Release\net10.0
-$p = Start-Process -PassThru -FilePath "dotnet" `
-    -ArgumentList "Lisp.dll bench.ss" `
-    -RedirectStandardOutput "bench_out.txt" `
-    -RedirectStandardError "bench_err.txt" `
-    -NoNewWindow
-$p.WaitForExit()
-Get-Content bench_out.txt | Select-String "time:|error"
