f0414b9175215b10c148342766e2bdc1f404ebb4
Ilias Mavreas <ilias.mavreas@gmail.com>
2026-04-09 21:45:08 -0500

    publish file update

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
-dotnet publish /p:PublishProfile=win-x64
-dotnet publish /p:PublishProfile=linux-x64
-dotnet publish /p:PublishProfile=osx-x64
-dotnet publish /p:PublishProfile=osx-arm64
+dotnet publish -c Release /p:PublishProfile=win-x64
+dotnet publish -c Release /p:PublishProfile=linux-x64
+dotnet publish -c Release /p:PublishProfile=osx-x64
+dotnet publish -c Release /p:PublishProfile=osx-arm64
